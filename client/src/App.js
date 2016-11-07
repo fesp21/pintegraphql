fde3b64c50e5ad683791b919d129bac1c6e2ea4f
Federico Marcos <marcosfede@gmail.com>
2016-11-07 12:49:56 -0300

    added settings_secret

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -14,8 +14,8 @@ class App extends Component {
             <input type="submit" value="Login" />
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
